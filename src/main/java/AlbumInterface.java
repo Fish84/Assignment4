4ae505302ad0305f2907bc27d240c05c91770c48
Fish84 <craigacarr@gmail.com>
2020-03-18 15:10:02 +0200

    Initial commit for Assignment 4

---
+++ src/main/java/AlbumInterface.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public interface AlbumInterface {
+
+    public Set<String> ReleaseDate();
+
+    public Map<String, String> Artists();
+
+    public List<String> AlbumName();
+
+}
